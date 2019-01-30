993c83e0b0cd8445769cc672b5008327ca6b5d67
Thiago Prado <trprado@outlook.com>
2019-01-30 19:54:27 -0200

    First mini_project naive_bayes and update tools to python3

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,15 +1,13 @@
-#!/usr/bin/python
-
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -22,12 +20,32 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
 
+from sklearn.naive_bayes import GaussianNB
 
+clf = GaussianNB()
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print("tempo de treinamento:", round(time()-t0, 3), "s")
+
+t0 = time()
+pred = clf.predict(features_test)
+print("tempo de predição:", round(time()-t0, 3), "s")
+
+from sklearn.metrics import accuracy_score
+
+accuracy = accuracy_score(labels_test, pred)
+print('Acurracia: ', round(accuracy, 4))
+
+# Results
 #########################################################
-
-
+# n nb_author_id.py
+# no. of Chris training emails: 7936
+# no. of Sara training emails: 7884
+# tempo de treinamento: 2.592 s
+# tempo de predição: 0.298 s
+# Acurracia:  0.9733
+#########################################################
--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -1,19 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 from nltk.stem.snowball import SnowballStemmer
-import string
 
 def parseOutText(f):
     """ given an opened email file f, parse out all text below the
         metadata block at the top
         (in Part 2, you will also add stemming capabilities)
         and return a string that contains all the words
-        in the email (space-separated) 
-        
+        in the email (space-separated)
+
         example use case:
         f = open("email_file_name.txt", "r")
         text = parseOutText(f)
-        
+
         """
 
 
@@ -25,7 +24,7 @@ def parseOutText(f):
     words = ""
     if len(content) > 1:
         ### remove punctuation
-        text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
+        text_string = content[1].translate(bytes.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
         words = text_string
@@ -33,22 +32,16 @@ def parseOutText(f):
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
-
-
 
 
     return words
 
-    
 
 def main():
     ff = open("../text_learning/test_email.txt", "r")
     text = parseOutText(ff)
-    print text
-
+    print(text)
 
 
 if __name__ == '__main__':
     main()
-

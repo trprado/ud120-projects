163537578244672da5bc570b6405579b0fc26544
Thiago Prado <trprado@outlook.com>
2019-02-05 22:02:42 -0200

    Add code to Decision Tree mini project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -24,8 +24,41 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print("tempo de treinamento:", round(time()-t0, 3), "s")
+
+t0 = time()
+pred = clf.predict(features_test)
+print("tempo de predição:", round(time()-t0, 3), "s")
+
+from sklearn.metrics import accuracy_score
+
+acc = accuracy_score(pred, labels_test)
+print(f'Accuracy: {acc}')
+
+# Quizz 2
+print(f'Features: {features_train.shape[1]}')
+
+# Results Quizz 1
+#########################################################
+# no. of Chris training emails: 7936
+# no. of Sara training emails: 7884
+# tempo de treinamento: 146.061 s
+# tempo de predição: 0.04 s
+# Accuracy: 0.9772468714448237
 #########################################################
 
-
+# Results Quizz 2
+#########################################################
+# no. of Chris training emails: 7936
+# no. of Sara training emails: 7884
+# tempo de treinamento: 146.061 s
+# tempo de predição: 0.04 s
+# Accuracy: 0.9772468714448237
+# Features: 3785
+#########################################################
